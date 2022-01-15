c65e3ccbbeaefb327c539e606fa5a461ef983509
Otto Jones <ottosjonesjr@gmail.com>
2022-01-15 11:39:15 -0500

    first push, listing some requirements needed to build game in index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// JAVASCRIPT
+
+// CARDS AND FUCTIONALITY
+
+// Need a function to create a deck of cards
+// Need a funtion to shuffle deck of cards
+// Need a funtion to render cards
+// Need way to keep track of dealers card total 
+// Need a way to keep track of players card total
+// Need a funtion to compare the value of dealers cards to players cards
+
+// GAME FUNCTIONALITY
+
+// Need a function to reset the game
+// Need to create way to bet 
+// Need to track how much money player has and end game when the player is out of money (How much money will the player start with)
+
+
+
+
+// CSS
+// Need to find images of cards to display
+// Need to decide on layout
+// Need to get background color
