80ed935aa4c49d6ae99a9d6c825804cb6742c90f
Otto Jones <ottosjonesjr@gmail.com>
2022-01-15 17:32:45 -0500

    added images of cards and began to style card

--- index.js
+++ index.js
@@ -24,8 +24,76 @@
 // Need to get background color
 
 // Array of suits
-const suits = ["spades", "diamonds", "clubs", "hearts"]
+const suits = ["Spades", "Diamonds", "Clubs", "Hearts"]
 
 // Array of card values
-const cardValues = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+
 // Function to create deck of cards
+
+function createDeck(arr) {
+    
+    let deck = []
+    let cards = []
+    for (let i = 0; i < 4; i++) {
+        let card = arr.map(value => ({Suit: suits[i], Value: value }))
+        cards.push(card)
+        // deck = cards.pop()
+     
+
+        
+        
+        
+    }
+    return cards
+}
+
+console.log(createDeck(values))
+
+
+// ({Value: value, Suit: suits[i]}))
+
+
+function getDeck()
+{
+    let deck = new Array();
+
+    for(let i = 0; i < suits.length; i++)
+    {
+        for(let x = 0; x < values.length; x++)
+        {
+            let card = {Value: values[x], Suit: suits[i]};
+            deck.push(card);
+        }
+    }
+
+    return deck;
+}
+// getDeck(deck)
+// console.log(getDeck(values))
+// let deck = getDeck(values)
+
+
+
+
+// function shuffle(deck)
+// {
+// 	// for 1000 turns
+// 	// switch the values of two random cards
+// 	for (let i = 0; i < 1000; i++)
+// 	{
+// 		let location1 = Math.floor((Math.random() * deck.length));
+// 		let location2 = Math.floor((Math.random() * deck.length));
+// 		let tmp = deck[location1];
+
+//         deck[location1] = deck[location2];
+//         // console.log(tmp)
+// 		deck[location2] = tmp;
+//     }
+//     return deck
+// }
+
+// console.log(shuffle(deck))
+
+let number = document.querySelector(".card-value")
+number.textContent = 7
