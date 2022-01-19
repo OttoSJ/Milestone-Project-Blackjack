558a1d3c7ba194af39aa031f750fa7b063d1cedf
Otto Jones <ottosjonesjr@gmail.com>
2022-01-18 19:42:18 -0500

    added checkForDoubleAces function. If two aces are dealt on the first hand it reduces the value of the hand to 12 which essentially forces one of the aces to have value of one so the player doesn't bust.

--- buildfunctions.js
+++ buildfunctions.js
@@ -1,127 +1,124 @@
-
-
-
 function renderNextCard(newCard, contestant) {
+  let cardDiv = document.createElement("div");
+  cardDiv.setAttribute("class", "card");
 
-    let cardDiv = document.createElement("div")
-    cardDiv.setAttribute("class", "card")
+  let playerCardsContainer = document.querySelector(
+    `.${contestant}-card-container`
+  );
 
-    let playerCardsContainer = document.querySelector(`.${contestant}-card-container`)
-    
-    let cardValueContainer = document.createElement("div")
-    cardValueContainer.setAttribute("class", "card-value-container")
-    
-    let playerCardValue = document.createElement("p")
-    playerCardValue.setAttribute("class", `${contestant}-card-value`)
-    playerCardValue.textContent = `${newCard.Value}` 
-    
-    let playerSuitContainer = document.createElement("div")
-    playerSuitContainer.setAttribute("class",`${contestant}-suit-container`)
-    
-    let image = document.createElement("img")
-    image.setAttribute("class", `${contestant}-suit`)
-    image.src = `images/${newCard.Suit}-solid.svg`
-    
-    playerCardsContainer.append(cardDiv)
-    cardDiv.append(cardValueContainer)
-    cardValueContainer.append(playerCardValue)
-    cardDiv.append(playerSuitContainer)
-    playerSuitContainer.append(image)
+  let cardValueContainer = document.createElement("div");
+  cardValueContainer.setAttribute("class", "card-value-container");
+
+  let playerCardValue = document.createElement("p");
+  playerCardValue.setAttribute("class", `${contestant}-card-value`);
+  playerCardValue.textContent = `${newCard.Value}`;
+
+  let playerSuitContainer = document.createElement("div");
+  playerSuitContainer.setAttribute("class", `${contestant}-suit-container`);
+
+  let image = document.createElement("img");
+  image.setAttribute("class", `${contestant}-suit`);
+  image.src = `images/${newCard.Suit}-solid.svg`;
+
+  playerCardsContainer.append(cardDiv);
+  cardDiv.append(cardValueContainer);
+  cardValueContainer.append(playerCardValue);
+  cardDiv.append(playerSuitContainer);
+  playerSuitContainer.append(image);
 }
 
-
 function createCard(newCard, contestant) {
-   
-    
-    let cardDiv = document.createElement("div")
-    cardDiv.setAttribute("class", "card")
-    
-    let cardsContainer = document.querySelector(`.${contestant}-card-container`)
-    
-    let cardValueContainer = document.createElement("div")
-    cardValueContainer.setAttribute("class", "card-value-container")
-    
-    let CardValue = document.createElement("p")
-    CardValue.setAttribute("class", `${contestant}-cards-value`)
-    CardValue.textContent = `${newCard.Value}` 
-    
-    let SuitContainer = document.createElement("div")
-    SuitContainer.setAttribute("class",`${contestant}-suit-container`)
-    
-    let image = document.createElement("img")
-    image.setAttribute("class", `${contestant}-suit`)
-    image.src = `images/${newCard.Suit}-solid.svg`
-    
-    cardsContainer.append(cardDiv)
-    cardDiv.append(cardValueContainer)
-    cardValueContainer.append(CardValue)
-    cardDiv.append(SuitContainer)
-    SuitContainer.append(image)
+  let cardDiv = document.createElement("div");
+  cardDiv.setAttribute("class", "card");
+
+  let cardsContainer = document.querySelector(`.${contestant}-card-container`);
+
+  let cardValueContainer = document.createElement("div");
+  cardValueContainer.setAttribute("class", "card-value-container");
+
+  let CardValue = document.createElement("p");
+  CardValue.setAttribute("class", `${contestant}-cards-value`);
+  CardValue.textContent = `${newCard.Value}`;
+
+  let SuitContainer = document.createElement("div");
+  SuitContainer.setAttribute("class", `${contestant}-suit-container`);
+
+  let image = document.createElement("img");
+  image.setAttribute("class", `${contestant}-suit`);
+  image.src = `images/${newCard.Suit}-solid.svg`;
+
+  cardsContainer.append(cardDiv);
+  cardDiv.append(cardValueContainer);
+  cardValueContainer.append(CardValue);
+  cardDiv.append(SuitContainer);
+  SuitContainer.append(image);
 }
 
 function createHitMeBtn() {
-    let button = document.createElement("button")
-    button.setAttribute("id", "hit-me")
-    button.setAttribute("onclick", "handleNewCard(event)")
-    button.setAttribute("class", "deal-card")
-    button.textContent = "Hit Me"
-    let main = document.querySelector("main")
-    main.append(button)
+  let button = document.createElement("button");
+  button.setAttribute("id", "hit-me");
+  button.setAttribute("onclick", "handleNewCard(event)");
+  button.setAttribute("class", "deal-card");
+  button.textContent = "Hit Me";
+  let main = document.querySelector("main");
+  main.append(button);
 }
 
 // Function checks the value of the card that was delt and assigns a value to face cards
 function getValue(element) {
-    if (element === "A") {
-        return 11
-    }else if (element === "J" ) {
-       return 10
-    } else if (element === "Q") {
-        return 10
-    } else if (element === "K") {
-        return 10
-    } else return Number(element)
-        
+  if (element === "A") {
+    return 11;
+  } else if (element === "J") {
+    return 10;
+  } else if (element === "Q") {
+    return 10;
+  } else if (element === "K") {
+    return 10;
+  } else return Number(element);
 }
 
 // Function check the total value of both the dealer and player's hands
 function checkValue(total) {
-   return total.reduce((total, element) => total + element)
+  return total.reduce((total, element) => total + element);
 }
-    
+
+function checkForDoubleAces(contestant, handValue) {
+  if (contestant.length === 2 && handValue >= 22) {
+    return console.log(handValue - 10);
+  }
+}
+
 // Function compares hands and returns the winner
 function compareHands() {
-    let player = checkValue(playersHand)
-    let dealer = checkValue(dealersHand)
-    if (player < dealer) {
-        return dealer + " Dealer Wins!"
-    }else return player + " Player Wins!"
+  let player = checkValue(playersHand);
+  let dealer = checkValue(dealersHand);
+  if (player < dealer) {
+    return dealer + " Dealer Wins!";
+  } else return player + " Player Wins!";
 }
-
 
 function checkForBlackjack(contestentsHand, contestant) {
-     if (checkValue(contestentsHand) === 21) {
-       return console.log(contestant)
-    }
+  if (contestentsHand === 21) {
+    return console.log(`${contestant} Has Blackjack!`);
+  } else if (contestentsHand > 21) {
+    return console.log(`${contestant} Loses!!`);
+  } else console.log(`No Blackjack ${contestant}`);
 }
-
-
 
 // THESE FUNCTIONS BELOW ARE NOT IN USE AS IM STILL WORKING ON THEM OR WORKING ON HOW TO EMPLEMENT THEM
 // ********************************************************************************
 
 // This function will be to replace the repition in the handleDealer function once I can figure out how to implement it
 function firstHand(shuffledDeck) {
-    for (let i = 0; i < 2; i++) {
-        let hand = []
-        let card = shuffledDeck.shift()
-        hand.push(card)
-    }
-    return hand
+  for (let i = 0; i < 2; i++) {
+    let hand = [];
+    let card = shuffledDeck.shift();
+    hand.push(card);
+  }
+  return hand;
 }
 
-
-// THIS IS MY FUNCTION TO CREATE A DECK OF CARDS BUT IT'S INCOMPLETE. CURRENTLY IT GIVES ME AN ARRAY WITH FOUR ARRAYS OF ALL THE SUITS. STILL NEED TO FIGURE OUT HOW TO COMBINED THEM INTO ONE ARRAY. 
-
+// THIS IS MY FUNCTION TO CREATE A DECK OF CARDS BUT IT'S INCOMPLETE. CURRENTLY IT GIVES ME AN ARRAY WITH FOUR ARRAYS OF ALL THE SUITS. STILL NEED TO FIGURE OUT HOW TO COMBINED THEM INTO ONE ARRAY.
 
 // function createDeck(arr) {
 //     let cards = []
@@ -133,4 +130,3 @@ function firstHand(shuffledDeck) {
 //     return cards
 // console.log(createDeck(values))
 // }
-
--- index.js
+++ index.js
@@ -1,200 +1,165 @@
-
-
-
-
-
-
-
 // JAVASCRIPT
 // CARDS AND FUCTIONALITY
 
-// Need way to keep track of dealers card total 
-// Need a way to keep track of players card total
-// Need to compare the value of dealers cards to players cards
+// Need to keep dealer from taking another card after 16
 
 // GAME FUNCTIONALITY
-
 
 // Need to create way to bet and track money
 // Need to track how much money player has and end game when the player is out of money (How much money will the player start with)
 // Need to add setTimeOut for slight delay for dealers card
 // Need to add message if dealer holds
 
-
-
 // CSS
 
-
-
-
-
-
-
-
 // Array of suits
-const suits = ["Spades", "Diamonds", "Clubs", "Hearts"]
+const suits = ["Spades", "Diamonds", "Clubs", "Hearts"];
 
 // Array of card values
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+const values = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K",
+];
 
 // Function to create deck of cards
 function getDeck() {
-    let deck = new Array();
+  let deck = new Array();
 
-    for(let i = 0; i < suits.length; i++)
-    {
-        for(let x = 0; x < values.length; x++)
-        {
-            let card = {Value: values[x], Suit: suits[i]};
-            deck.push(card);
-        }
+  for (let i = 0; i < suits.length; i++) {
+    for (let x = 0; x < values.length; x++) {
+      let card = { Value: values[x], Suit: suits[i] };
+      deck.push(card);
     }
+  }
 
-    return deck;
+  return deck;
 }
 // console.log(getDeck(values))
 
-
 // Function to shuffle cards
-function shuffle(deck)
-{
-	
-	for (let i = 0; i < 1000; i++)
-	{
-		let location1 = Math.floor((Math.random() * deck.length));
-		let location2 = Math.floor((Math.random() * deck.length));
-		let tmp = deck[location1];
+function shuffle(deck) {
+  for (let i = 0; i < 1000; i++) {
+    let location1 = Math.floor(Math.random() * deck.length);
+    let location2 = Math.floor(Math.random() * deck.length);
+    let tmp = deck[location1];
 
-        deck[location1] = deck[location2];
-        // console.log(tmp)
-		deck[location2] = tmp;
-    }
-    return deck
+    deck[location1] = deck[location2];
+
+    deck[location2] = tmp;
+  }
+  return deck;
 }
-
 
 // Function to deal player card from top of deck with dealer function inside
 
-const playersHand = []
-const dealersHand = []
-const deck = getDeck(values)
-const shuffledDeck = shuffle(deck)
-console.log(shuffledDeck)
-// I hate how repetive this function is!!!!!!! 
+const playersHand = [];
+const dealersHand = [];
+const deck = getDeck(values);
+const shuffledDeck = shuffle(deck);
+console.log(shuffledDeck);
+// I hate how repetive this function is!!!!!!!
 function handleDealer(event) {
-    event.preventDefault()
-    const dealer = "dealer"
-    const player = "player"
+  event.preventDefault();
+  const dealer = "dealer";
+  const player = "player";
 
-    let firstPlayerCard = shuffledDeck.shift()
-    let secondPlayerCard = shuffledDeck.shift()
-    let firstDealerCard = shuffledDeck.shift()
-    let secondDealerCard = shuffledDeck.shift()
-    let firstPlayerCardValue = getValue(firstPlayerCard.Value)
-    let secondPlayerCardValue = getValue(secondPlayerCard.Value)
-    let firstDealerCardValue = getValue(firstDealerCard.Value)
-    let secondDealerCardValue = getValue(secondDealerCard.Value)
-    playersHand.push(firstPlayerCardValue, secondPlayerCardValue)
-    dealersHand.push(firstDealerCardValue, secondDealerCardValue)
-    createCard(firstPlayerCard, player)
-    createCard(secondPlayerCard, player)
-    createCard(firstDealerCard , dealer)
-    createCard(secondDealerCard, dealer)
-    checkForBlackjack(dealersHand, dealer)
-    checkForBlackjack(playersHand, player)
-    createHitMeBtn()
-    document.getElementById("hit-me").style.zIndex = 4
-    let hold = document.createElement("button")
-    hold.setAttribute("class", "hold")
-    hold.setAttribute("onclick", "playerHold()")
-    hold.textContent = "Hold"
-    hold.style.zIndex = 3
-    document.body.append(hold)
-    console.log(dealersHand)
-    console.log(playersHand)
-   
-    console.log(checkValue(dealersHand))
-    console.log(checkValue(playersHand))
+  let firstPlayerCard = shuffledDeck.shift();
+  let secondPlayerCard = shuffledDeck.shift();
+  let firstDealerCard = shuffledDeck.shift();
+  let secondDealerCard = shuffledDeck.shift();
+  let firstPlayerCardValue = getValue(firstPlayerCard.Value);
+  let secondPlayerCardValue = getValue(secondPlayerCard.Value);
+  let firstDealerCardValue = getValue(firstDealerCard.Value);
+  let secondDealerCardValue = getValue(secondDealerCard.Value);
+  playersHand.push(firstPlayerCardValue, secondPlayerCardValue);
+  dealersHand.push(firstDealerCardValue, secondDealerCardValue);
+  let dealersHandTotal = checkValue(dealersHand);
+  let playersHandTotal = checkValue(playersHand);
+
+  createCard(firstPlayerCard, player);
+  createCard(secondPlayerCard, player);
+  createCard(firstDealerCard, dealer);
+  createCard(secondDealerCard, dealer);
+  checkForBlackjack(dealersHandTotal, dealer);
+  checkForBlackjack(playersHandTotal, player);
+  checkForDoubleAces(playersHand, playersHandTotal);
+  checkForDoubleAces(dealersHand, dealersHandTotal);
+  createHitMeBtn();
+  document.getElementById("hit-me").style.zIndex = 4;
+  let hold = document.createElement("button");
+  hold.setAttribute("class", "hold");
+  hold.setAttribute("onclick", "playerHold()");
+  hold.textContent = "Hold";
+  hold.style.zIndex = 3;
+  document.body.append(hold);
+  // console.log(dealersHand)
+  // console.log(playersHand)
+
+  console.log(checkValue(dealersHand));
+  console.log(checkValue(playersHand));
 }
 
 function playerHold() {
-    let hitMeButton = document.getElementById("hit-me")
-    hitMeButton.remove()
-    let results = compareHands()
-  
-    console.log(results)
+  let hitMeButton = document.getElementById("hit-me");
+  hitMeButton.remove();
+  let results = compareHands();
+
+  console.log(results);
 }
 
 function handleNewCard(event) {
-    event.preventDefault()
-    const dealer = "dealer"
-    const player = "player"
+  event.preventDefault();
+  const dealer = "dealer";
+  const player = "player";
 
-    let nextPlayerCard = shuffledDeck.shift()
-    let nextPlayerCardValue = getValue(nextPlayerCard.Value)
+  let nextPlayerCard = shuffledDeck.shift();
+  let nextPlayerCardValue = getValue(nextPlayerCard.Value);
 
-    let nextDealerCard = shuffledDeck.shift()
-    let nextDealerCardValue = getValue(nextDealerCard.Value)
-    
-    playersHand.push(nextPlayerCardValue)
-    dealersHand.push(nextDealerCardValue)
-    renderNextCard(nextPlayerCard, dealer)
-    renderNextCard(nextDealerCard, player)
-    
-    let dealersHandTotal = checkValue(dealersHand)
-    let playersHandTotal = checkValue(playersHand)
-    console.log(dealersHandTotal)
-    console.log(playersHandTotal)
-    
+  let nextDealerCard = shuffledDeck.shift();
+  let nextDealerCardValue = getValue(nextDealerCard.Value);
+
+  playersHand.push(nextPlayerCardValue);
+  dealersHand.push(nextDealerCardValue);
+  renderNextCard(nextPlayerCard, dealer);
+  renderNextCard(nextDealerCard, player);
+
+  let dealersHandTotal = checkValue(dealersHand);
+  let playersHandTotal = checkValue(playersHand);
+
+  checkForBlackjack(dealersHandTotal, dealer);
+  checkForBlackjack(playersHandTotal, player);
+  console.log(dealersHandTotal);
+  console.log(playersHandTotal);
 }
-    
-   
+
 // Function that resets the game
-function handleReset() { 
-    let el = document.getElementsByClassName("card")
-    let hitMeButton = document.getElementById("hit-me")
+function handleReset() {
+  let el = document.getElementsByClassName("card");
+  let hitMeButton = document.getElementById("hit-me");
 
-    if (el.length > 0) {
-        for (let i = 0; i < el.length; i++) {
-            el[i].remove()
-            handleReset()
-            
-        }
+  if (el.length > 0) {
+    for (let i = 0; i < el.length; i++) {
+      el[i].remove();
+      handleReset();
     }
-    // Need to figure out a better solution for hiding this button. This creates a button everytime this has to run to delete the cards
-    let hold = document.createElement("button")
-    hold.setAttribute("class", "cover-hold")
-    document.body.append(hold)
+  }
+  // Need to figure out a better solution for hiding this button. This creates a button everytime this has to run to delete the cards
+  let hold = document.createElement("button");
+  hold.setAttribute("class", "cover-hold");
+  document.body.append(hold);
 
-    hitMeButton.remove()
-    playersHand.length = 0;
-    dealersHand.length = 0;
-    
+  hitMeButton.remove();
+  playersHand.length = 0;
+  dealersHand.length = 0;
 }
-   
-
-
-   
-
-
-
-    
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
