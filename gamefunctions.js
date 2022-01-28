36dfad0c15820121e32b2749774762ff535a8b54
Otto Jones <ottosjonesjr@gmail.com>
2022-01-27 20:31:49 -0500

    changed media queries for smaller screen by moving the button to the right side of the screen.

--- gamefunctions.js
+++ gamefunctions.js
@@ -245,11 +245,8 @@ function handleReset() {
 // TO DO LIST
 
 // Need to fix bug that causes -10 when there are 5 cards and no aces and over 21. Here is the line in the function that needs to be rethought (else if (numberOfAces + playersHand.length === 5 && handTotal > 21) {
-// return handTotal - 10;). The problem is that its looking for the total of 5 cards which doesn't have include anything from the aces count
-
-// Need to work on Media for smaller devices. Make sure to account for screen to be turned sideways as well.
+// return handTotal - 10;). The problem is that its looking for the total of 5 cards which doesn't have include anything from the aces count, thus once you get to five cards and the total is over the set amount it takes off 10.
 
 // Need to add a feature that checks and creates another deck once the current deck is near epmty. It should be added to the play again button which is called be the handleReset function.
 
-// Need to add read me file. Find info on the internet and use some of what you find.
-console.log(deck);
+// Need to add read me file.
